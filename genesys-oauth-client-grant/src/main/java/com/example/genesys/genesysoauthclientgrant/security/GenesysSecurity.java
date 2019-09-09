1463539ebed4cbf3d1f7946d4ccd27d37279bcf9
rkumar9090 <rkumar9090@hotmail.com>
2019-09-09 09:21:22 +0530

    datasource

--- genesys-oauth-client-grant/src/main/java/com/example/genesys/genesysoauthclientgrant/security/GenesysSecurity.java
+++ genesys-oauth-client-grant/src/main/java/com/example/genesys/genesysoauthclientgrant/security/GenesysSecurity.java
@@ -1,7 +1,13 @@
 package com.example.genesys.genesysoauthclientgrant.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -9,8 +15,12 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
+@PropertySource({ "classpath:application.properties" })
 @EnableAuthorizationServer
 public class GenesysSecurity extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	private Environment env;
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
@@ -21,8 +31,18 @@ public class GenesysSecurity extends AuthorizationServerConfigurerAdapter {
 	}
 
 	@Override
-	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("genesys").secret("secret").authorizedGrantTypes("client_credentials")
-				.scopes("resource-server-read", "resource-server-write");
+	public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.jdbc(dataSource());
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+		dataSource.setUsername(env.getProperty("jdbc.user"));
+		dataSource.setPassword(env.getProperty("jdbc.pass"));
+		return dataSource;
 	}
 }
